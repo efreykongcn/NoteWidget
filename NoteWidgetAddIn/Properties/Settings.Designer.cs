ea5ed64febac31d63fe3e16edfbe664e0ae19481
Efrey Kong <45627994+efreykongcn@users.noreply.github.com>
2022-06-24 23:34:48 +0800

    Auto refresh Preview Window

--- NoteWidgetAddIn/Properties/Settings.Designer.cs
+++ NoteWidgetAddIn/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace NoteWidgetAddIn.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -128,6 +128,18 @@ namespace NoteWidgetAddIn.Properties {
             }
             set {
                 this["Markdown_Preview_Top"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        public int Markdown_PreviewRefresh_Interval {
+            get {
+                return ((int)(this["Markdown_PreviewRefresh_Interval"]));
+            }
+            set {
+                this["Markdown_PreviewRefresh_Interval"] = value;
             }
         }
     }
